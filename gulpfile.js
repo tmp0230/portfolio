60bda6271fc27998f532bab3dc2655d6a2edacc9
Arthur <arthur@muchir.fr>
2014-05-21 18:41:05 +0200

    change gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,44 +2,33 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     path = require('path'),
 
-    clean = require('gulp-clean'),
-    swig = require('gulp-swig-precompile'),
-    nodemon = require('gulp-nodemon');
-
+    clean = require('gulp-clean');
 
 // Templates
 // =========
-
-gulp.task('swig', function(){
-    return gulp.src('templates/**/*.html', {base: path.join(__dirname, 'templates')})
-        .pipe(swig())
-        .pipe(gulp.dest('jst'));
-});
 
 
 // Watch
 // =====
 
-gulp.task('watch', function(){
-    return nodemon({script: 'server/server.js', ext: 'js'})
-        .on('restart', function(){
-            console.log('server restarted');
-        });
-});
-
 
 // Clean
 // =====
 
-gulp.task('superclean', function(){
+gulp.task('clean', function(){
     return gulp.src('jst', {read: false})
         .pipe(clean());
 });
 
-
 // Tasks
 // =====
 
-gulp.task('default', ['swig', 'watch']);
+//gulp.task('default', ['watch']);
 
-gulp.task('clean', ['superclean']);
+gulp.task('build', ['clean'], function(){
+    gulp.start('postbuild');
+});
+
+gulp.task('postbuild', ['template'], function(){
+    process.exit();
+});
