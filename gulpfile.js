8f55ca921ab01abd0341c649e666171353e16da2
Arthur <arthur@muchir.fr>
2014-05-27 21:22:38 +0200

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
 
     clean = require('gulp-clean'),
+    rename = require('gulp-rename'),
     nunjucks = require('gulp-nunjucks'),
     processhtml = require('gulp-processhtml');
 
@@ -14,11 +15,29 @@ gulp.task('templates', function(){
         .pipe(gulp.dest('public/jst'));
 });
 
-gulp.task('templatesProcess', function(){
+gulp.task('templateCopy', function(){
     return gulp.src('templates/layout/base.html')
-        .pipe(gulp.dest('templates/layout/base.html.original'))
+        .pipe(rename({prefix: 'orig.'}))
+        .pipe(gulp.dest('templates/layout'))
         .pipe(processhtml('base.html'))
         .pipe(gulp.dest('templates/layout'));
+});
+
+gulp.task('templateCompile', ['templateCopy'], function(){
+    return gulp.src(['!templates/layout/orig.base.html', 'templates/**/*.html'])
+        .pipe(nunjucks())
+        .pipe(gulp.dest('public/jst'));
+});
+
+gulp.task('templateRename', ['templateCompile'], function(){
+    return gulp.src('templates/layout/orig.base.html')
+        .pipe(rename('base.html'))
+        .pipe(gulp.dest('templates/layout'));
+});
+
+gulp.task('templateClean', ['templateRename'], function(){
+    return gulp.src('templates/layout/orig.base.html', {read: false})
+        .pipe(clean());
 });
 
 // Watch
@@ -32,7 +51,7 @@ gulp.task('watch', ['templates'], function(){
 // =====
 
 gulp.task('clean', function(){
-    return gulp.src('public/jst', {read: false})
+    return gulp.src(['public/jst', '**/.DS_Store'], {read: false})
         .pipe(clean());
 });
 
@@ -45,6 +64,6 @@ gulp.task('build', ['clean'], function(){
     gulp.start('postbuild');
 });
 
-gulp.task('postbuild', ['template'], function(){
+gulp.task('postbuild', ['templateClean'], function(){
     process.exit();
 });
