8f0a46265c54063cb96cac8ef36258ee66d28a27
Arthur <arthur@muchir.fr>
2014-06-18 18:48:32 +0200

    add digest service

--- public/app/js/admin/controllers/login.js
+++ public/app/js/admin/controllers/login.js
@@ -1,111 +1,21 @@
 'use strict';
 
 angular.module('myApp.controllers')
-    .controller('LoginController', ['$scope', function($scope){
+    .controller('LoginController', ['$scope', 'DigestAuth', function($scope, DigestAuth){
 
         var FORM_URL = '/login/',
             REDIRECT_TO = '/projects/';
 
-        var nc = 1,
-            realm = null,
-            nonce = null,
-            cnonce = null;
-
-        var askHeaderToken = function(){
-
-            $http({
-                method: 'POST',
-                url: FORM_URL
-            }).success(function(data, status, headers){
-                if(status === 200){
-                    $location.path(REDIRECT_TO);
-                }
-                else if(status === 401){
-                    clientAuth(headers);
-                }
-            });
-        };
-
-        var clientAuth = function(headers){
-
-            var email = $scope.input.email,
-                passwordHash = $.md5($scope.input.password);
-
-            generateClientNonce();
-
-            if(realm === null && nonce === null) extractInfoHeader(headers);
-
-            var response = generateResponse(email, passwordHash),
-                nc = ('00000000'+nc).slice(-8),
-                digestHeader =  'Digest username="'+email+'", '+
-                                'realm="'+realm+'", '+
-                                'nonce="'+nonce+'", '+
-                                'uri="'+FORM_URL+'", '+
-                                'qop=auth, '+
-                                'nc='+nc+', '+
-                                'cnonce="'+cnonce+'", '+
-                                'response="'+response+'"';
-
-            $http({
-                method: 'POST',
-                url: FORM_URL,
-                headers: {
-                    'Authorization': digestHeader
-                }
-            }).success(function(data, status, headers){
-                if(status === 200){
-                    $location.path(REDIRECT_TO);
-                }
-                else if(status === 401){
-                    extractInfoHeader(headers);
-                }
-            });
-        };
-
-        var generateClientNonce = function(){
-
-            var characters = 'abcdef0123456789',
-                charLength = characters.length,
-                token = '',
-                randNum = null;
-
-            for(var i=0; i<16; i++){
-                randNum = Math.round(Math.random() * charLength);
-                token += characters.substr(randNum, 1);
-            }
-
-            cnonce = token;
-        };
-
-        var extractInfoHeader = function(headers){
-
-            var responseHeaders = headers.getResponseHeader('WWW-Authenticate'),
-                realmRegex = /realm="(.*?)"/i,
-                nonceRegex = /nonce="(.*?)"/i;
-
-            realm = realmRegex.exec(responseHeaders)[1];
-            nonce = nonceRegex.exec(responseHeaders)[1];
-        };
-
-        var generateResponse = function(email, password){
-
-            var ha1 = $.md5(email+':'+realm+':'+password),
-                ha2 = $.md5('POST:'+FORM_URL),
-                nc = ('00000000'+nc).slice(-8);
-
-            return $.md5(ha1+':'+nonce+':'+nc+':'+cnonce+':auth:'+ha2);
-        };
-
-// $scope
-// ======
+        // $scope
+        // ======
 
         $scope.submitForm = function(){
 
-            if(realm === null && nonce === null){
-                askHeaderToken();
+            if(!this.headerExtracted){
+                DigestAuth.askHeaderToken(FORM_URL, REDIRECT_TO, $scope.input.email, $scope.input.password);
             }
             else{
-                clientAuth();
+                DigestAuth.clientAuth(null, FORM_URL, REDIRECT_TO, $scope.input.email, $scope.input.password);
             }
-        }
+        };
     }]);
--- public/app/js/admin/services/digest.js
+++ public/app/js/admin/services/digest.js
@@ -1,6 +1,103 @@
 'use strict';
 
 angular.module('myApp.services')
-    .factory('DigestAuth', ['$http', function($http){
+    .sercice('DigestAuth', ['$http', '$location', function($http, $location){
 
+        var nc = 1,
+            realm = null,
+            nonce = null,
+            cnonce = null;
+
+        var generateClientNonce = function(){
+
+            var characters = 'abcdef0123456789',
+                charLength = characters.length,
+                token = '',
+                randNum = null;
+
+            for(var i=0; i<16; i++){
+                randNum = Math.round(Math.random() * charLength);
+                token += characters.substr(randNum, 1);
+            }
+
+            cnonce = token;
+        };
+
+        var extractInfoHeader = function(headers){
+
+            var responseHeaders = headers('WWW-Authenticate'),
+                realmRegex = /realm="(.*?)"/i,
+                nonceRegex = /nonce="(.*?)"/i;
+
+            realm = realmRegex.exec(responseHeaders)[1];
+            nonce = nonceRegex.exec(responseHeaders)[1];
+        };
+
+        var generateResponse = function(email, password, FORM_URL){
+
+            var ha1 = $.md5(email+':'+realm+':'+password),
+                ha2 = $.md5('POST:'+FORM_URL),
+                ncSlice = ('00000000'+nc).slice(-8);
+
+            return $.md5(ha1+':'+nonce+':'+ncSlice+':'+cnonce+':auth:'+ha2);
+        };
+
+        // Public
+        // ======
+
+        this.headerExtracted = false;
+
+        this.askHeaderToken = function(FORM_URL, REDIRECT_TO, email, password){
+
+            $http({
+                method: 'POST',
+                url: FORM_URL
+            }).success(function(data, status, headers){
+                if(status === 200){
+                    $location.path(REDIRECT_TO);
+                }
+                else if(status === 401){
+                    this.clientAuth(headers, FORM_URL, REDIRECT_TO, email, password);
+                }
+            });
+        };
+
+        this.clientAuth = function(headers, FORM_URL, REDIRECT_TO, email, password){
+
+            var passwordHash = $.md5(password);
+
+            generateClientNonce();
+
+            if(realm === null && nonce === null){
+                extractInfoHeader(headers);
+
+                this.headerExtracted = true;
+            }
+
+            var response = generateResponse(email, passwordHash, FORM_URL),
+                nc = ('00000000'+nc).slice(-8),
+                digestHeader =  'Digest username="'+email+'", '+
+                                'realm="'+realm+'", '+
+                                'nonce="'+nonce+'", '+
+                                'uri="'+FORM_URL+'", '+
+                                'qop=auth, '+
+                                'nc='+nc+', '+
+                                'cnonce="'+cnonce+'", '+
+                                'response="'+response+'"';
+
+            $http({
+                method: 'POST',
+                url: FORM_URL,
+                headers: {
+                    'Authorization': digestHeader
+                }
+            }).success(function(data, status, headers){
+                if(status === 200){
+                    $location.path(REDIRECT_TO);
+                }
+                else if(status === 401){
+                    extractInfoHeader(headers);
+                }
+            });
+        };
     }]);
